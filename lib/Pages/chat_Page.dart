a9492fc0e963d59d7085d21ae47b8804b4ebdb60
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2023-10-23 00:40:06 +0300

    Added FirebaseFireStore

--- lib/Pages/chat_Page.dart
+++ lib/Pages/chat_Page.dart
@@ -1,52 +1,76 @@
+// ignore_for_file: use_key_in_widget_constructors, must_be_immutable
+
 import 'package:chat_app/Widgets/Chat_Bubble.dart';
 import 'package:chat_app/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   static String id = 'Chat Page';
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(kMessagesCollections);
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: kPrimaryColor,
-          centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/RedSus.png',
-                height: 50,
-              ),
-              const Text('Chat')
-            ],
-          ),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return const chatBubble();
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Send Message ',
-                  suffixIcon: Icon(
-                    Icons.send,
-                    color: kPrimaryColor,
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data!.docs[0]['message']);
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: kPrimaryColor,
+              centerTitle: true,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/RedSus.png',
+                    height: 50,
                   ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: BorderSide(color: kPrimaryColor)),
-                ),
+                  const Text('Chat')
+                ],
               ),
             ),
-          ],
-        ));
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return const chatBubble();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (value) {
+                      messages.add(
+                        {'message': value},
+                      );
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Send Message ',
+                      suffixIcon: const Icon(
+                        Icons.send,
+                        color: kPrimaryColor,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: const BorderSide(color: kPrimaryColor)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else {
+          return Text('Loading ...');
+        }
+      },
+    );
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff2B475E);
+const kMessagesCollections = 'messages';
